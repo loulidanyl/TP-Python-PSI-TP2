748f4801276f92b0741d0e5fa23afb121bcfbbe1
loulidanyl <45450321+loulidanyl@users.noreply.github.com>
2018-12-17 10:28:25 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+"""Main module for the Python Airport code"""
+
+from PyQt5 import QtWidgets
+import airport
+import traffic
+import simulation
+import radarview
+
+APT_FILE = ("DATA/lfpg_map.txt", "DATA/lfpo_map.txt")
+PLN_FILE = ("DATA/lfpg_flights.txt", "DATA/lfpo_flights.txt")
+
+if __name__ == "__main__":
+    # Load files
+    # choice = 0 if input("1: Roissy / [2: Orly] ? ") == '1' else 1
+    choice = 0
+    apt = airport.from_file(APT_FILE[choice])
+    flights = traffic.from_file(apt, PLN_FILE[choice])
+
+    # create the simulation
+    sim = simulation.Simulation(apt, flights)
+
+    # Initialize Qt
+    app = QtWidgets.QApplication([])
+
+    # create the radar view and the time navigation interface
+    main_window = radarview.RadarView(sim)
+    second_window = radarview.PanZoomView(main_window.scene)
+    second_window.scale(0.1,-0.1)
+    second_window.show()
+    print(simulation.SHORTCUTS)
+
+    # enter the main loop
+    app.exec_()
